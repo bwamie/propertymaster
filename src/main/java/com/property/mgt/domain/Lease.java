01972ca51c79a00829b7dad9e0b9c9c957410da5
bwamie <bwamie@gmail.com>
2017-03-21 14:27:15 -0500

    Afternoon1

--- src/main/java/com/property/mgt/domain/Lease.java
+++ src/main/java/com/property/mgt/domain/Lease.java
@@ -1,10 +1,15 @@
 package com.property.mgt.domain;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Lease {
@@ -13,5 +18,44 @@ public class Lease {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="id")
 	private long leadId;
-
+	
+	private Date startDate;
+	private Date endDate;
+	private double monthlyRate;
+	@OneToOne(fetch=FetchType.EAGER, cascade=CascadeType.ALL) 
+	private Client client;
+	
+	public long getLeadId() {
+		return leadId;
+	}
+	public void setLeadId(long leadId) {
+		this.leadId = leadId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public double getMonthlyRate() {
+		return monthlyRate;
+	}
+	public void setMonthlyRate(double monthlyRate) {
+		this.monthlyRate = monthlyRate;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
+	
 }
--- src/main/java/com/property/mgt/domain/Preference.java
+++ src/main/java/com/property/mgt/domain/Preference.java
@@ -1,10 +1,15 @@
 package com.property.mgt.domain;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Preference {
@@ -15,5 +20,53 @@ public class Preference {
 	private long preferenceId;
 	private String preferenceType;
 	private double maxRent;
+	@OneToOne(fetch=FetchType.EAGER, cascade=CascadeType.ALL) 
+	private Client client;
+	private Date date; 
+	
+	public Preference(){
+		
+	}
+
+	public long getPreferenceId() {
+		return preferenceId;
+	}
+
+	public void setPreferenceId(long preferenceId) {
+		this.preferenceId = preferenceId;
+	}
+
+	public String getPreferenceType() {
+		return preferenceType;
+	}
+
+	public void setPreferenceType(String preferenceType) {
+		this.preferenceType = preferenceType;
+	}
+
+	public double getMaxRent() {
+		return maxRent;
+	}
+
+	public void setMaxRent(double maxRent) {
+		this.maxRent = maxRent;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 	
 }
